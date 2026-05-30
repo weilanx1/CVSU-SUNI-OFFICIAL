56fd398320b241045cb41cd0c627bfa481ff1ffb
weilanx1 <xeedlove2080@gmail.com>
2026-05-30 23:17:44 +0800

    Added nav bar

--- host-events.php
+++ host-events.php
@@ -8,7 +8,22 @@
     <link rel="stylesheet" href="css/host.css">
 </head>
 <body>
-
+    <nav>
+          <a href="">
+          <img src="images/logo.png" alt="Suni Logo">
+          </a>
+          <ul> 
+               <li><a href="#" class="active">CvSU Events</a></li>
+               <li><a href="#" class="active">My Events</a></li>
+               <li><button class="search-btn">
+                <img src="images/search_24dp_E3E3E3_FILL0_wght400_GRAD0_opsz24.svg" alt="search" aria-hidden="true" style="width: 20px;">
+               </button></li>
+               <li><button class="search-btn">
+                <img src="images/search_24dp_E3E3E3_FILL0_wght400_GRAD0_opsz24.svg" alt="search" aria-hidden="true" style="width: 20px;">
+               </button></li>
+               <li><button class="nav-btn" onclick="window.location.href='sign-in.html'">Sign in</button></li>
+          </ul>
+     </nav>
 
      <div class="container">
           <div class="greenbox">
